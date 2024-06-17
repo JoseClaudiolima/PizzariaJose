f0d40ffc375480a81a1853bcefda4391de066d9e
José Cláudio <jose.c.lima.sp@gmail.com>
2024-06-17 10:04:16 -0300

    Adicioando Header e footer

---
+++ templates/footer.php
@@ -0,0 +1,11 @@
+<?php
+
+
+?>
+
+<footer>
+    <a href="https://github.com/JoseClaudiolima/testepizzariajoao" target="_blank">Pizzaria do José &copy; 2024</a>
+</footer>
+
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pizzaria</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!--Fonte Roboto -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    
+<header>
+    <a href=""> <img src="img/pizza.svg" alt=""> </a>
+    <a href="https://github.com/JoseClaudiolima/testepizzariajoao" target="_blank">
+        <p>Pizzaria do José</p>
+    </a>
+</header>
+
